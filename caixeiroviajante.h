c8d6b593e1df6c0616529556c85c15ea8191babe
Karyne <karybea@gmail.com>
2021-04-07 13:44:44 -0300

    Create caixeiroviajante.h

---
+++ caixeiroviajante.h
@@ -0,0 +1,29 @@
+void construir_caminho(struct matriz m, int* caminho) {
+    int *inseridos = malloc(m.numero_elementos * sizeof(int));
+
+    for(int i = 0; i < m.numero_elementos; i++) {
+        inseridos[i] = FALSE;
+    }
+
+    caminho[0] = 0;
+    inseridos[0] = TRUE;
+
+    for(int i = 0; i < m.numero_elementos; i++) {
+        int valor_referencia = INT_MAX;
+        int vizinho_selecionado = 0;
+
+        for(int j = 0; j < m.numero_elementos; j++) {
+            if(!inseridos[j] && valor_referencia > m.elementos[i][j]) {
+                vizinho_selecionado = j;
+                valor_referencia = m.elementos[i][j];
+            }
+        }
+
+        caminho[i + 1] = vizinho_selecionado;
+        inseridos[vizinho_selecionado] = TRUE;
+    }
+
+    caminho[m.numero_elementos] = 0;
+
+    free(inseridos);
+}
